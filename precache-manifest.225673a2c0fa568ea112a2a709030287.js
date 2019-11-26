0cb15cb52851eeff5b2b2f278f8984eae6727109
OsmanAbdulMoomin <o.abdulmoomin@gmail.com>
2019-11-26 17:08:42 +0000

    Updates

---
+++ precache-manifest.225673a2c0fa568ea112a2a709030287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4117a5b09bd58c4bb19e535436427afe",
+    "url": "/what-do-i-mean/index.html"
+  },
+  {
+    "revision": "fbf7799be679baef1ea8",
+    "url": "/what-do-i-mean/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bf19ae48d96af1cd0f00",
+    "url": "/what-do-i-mean/static/js/2.a95d2c52.chunk.js"
+  },
+  {
+    "revision": "fbf7799be679baef1ea8",
+    "url": "/what-do-i-mean/static/js/main.f647cd36.chunk.js"
+  },
+  {
+    "revision": "291b137b0a1ee1393d7a",
+    "url": "/what-do-i-mean/static/js/runtime-main.9ef0f689.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/what-do-i-mean/static/media/logo.25bf045c.svg"
+  }
+]);
